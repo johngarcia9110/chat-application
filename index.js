eaca63f3381704fd0cf2a04bd171329db1807717
John B Garcia <johngarcia9110@gmail.com>
2016-10-19 22:14:05 -0400

    added socket.io + hooks

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var express = require('express');
 var path = require('path');
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
